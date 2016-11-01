0dddc4d8e5d10fdb830787f3c785a7ea0461f1b1
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-11-01 16:36:37 +0300

    Application component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+import Application from './components/Application';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const application = (<div>Application!</div>);
+  const application = (<Application />);
   const element = document.querySelector('#application');
 
   ReactDOM.render(application, element);
---
+++ app/javascript/components/Application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Application extends React.Component {
+  render() {
+    return (<div>Application component!</div>);
+  }
+}
+
+export default Application;
